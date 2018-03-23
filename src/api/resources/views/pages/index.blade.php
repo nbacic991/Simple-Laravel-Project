d66ccf33d7be74872407b305f15c45c34e789eca
Nemanja Bacic <nbacic@ymail.com>
2018-03-23 13:23:01 +0100

    Added node modules, and starter layout with navigation.

--- src/api/resources/views/pages/index.blade.php
+++ src/api/resources/views/pages/index.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{ $title }}</h1>
-    <p>This is the laravel application from the "Laravel From Scratch" Youtube Series</p>
+    <div class="jumbotron text-center">
+        <h1>{{ $title }}</h1>
+        <p>This is the laravel application from the "Laravel From Scratch" Youtube Series</p>
+        <p>
+            <a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> 
+            <a class="btn btn-success btn-lg" href="/register">Register</a>
+        </p>
+    </div>
 @endsection
