6b5f08a05e4ab4751065d282d480496b58e6dc2f
Nemanja Bacic <nbacic@ymail.com>
2018-03-23 15:23:46 +0100

    Edit and delete functionality added.

--- src/api/app/Http/Controllers/PostsController.php
+++ src/api/app/Http/Controllers/PostsController.php
@@ -89,7 +89,8 @@ class PostsController extends Controller
      */
     public function edit($id)
     {
-        //
+        $post = Post::find($id);
+        return view('posts.edit')->with('post', $post);
     }
 
     /**
@@ -101,7 +102,19 @@ class PostsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        // Create Post
+        $post = Post::find($id);
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+
+        $post->save();
+
+        return redirect('/posts')->with('success', 'Post Updated');
     }
 
     /**
@@ -112,6 +125,9 @@ class PostsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+
+        return redirect('/posts')->with('success', 'Post Removed');
     }
 }
